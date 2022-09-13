3703e0b861a6715c4a4e32ca50dc19f96432a8f4
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-09-13 21:39:42 +0300

    Added handling for special cases

--- functions_intermediate_I/functions_intermediate_I.py
+++ functions_intermediate_I/functions_intermediate_I.py
@@ -2,8 +2,11 @@ import random
 
 
 def randInt(min=0, max=100):
-    num = round(random.random() * (max - min) + min)
-    return num
+    if min > max or max < 0:
+        return "Error:Invalid Max/Min Values"
+    else:
+        num = round(random.random() * (max - min) + min)
+        return num
 
 
 # print(randInt()) 		    # should print a random integer between 0 to 100
@@ -12,6 +15,8 @@ def randInt(min=0, max=100):
 # print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
 
 print(randInt())
+print(randInt(max=-1))
+print(randInt(200, 3))
 print(randInt(min=10))
 print(randInt(max=1000))
 print(randInt(15, 300))
