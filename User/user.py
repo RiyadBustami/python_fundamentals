c1f9733a700e9bb7f95d5b867318c414c97f74f9
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-09-21 21:47:15 +0300

    added chaining

--- User/user.py
+++ User/user.py
@@ -5,16 +5,20 @@ class User:
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
 
     def display_user_balance(self):
         print(self.name, self.balance)
+        return self
 
     def transfer_money(self, user, amount):
         self.make_withdrawal(amount)
         user.make_deposit(amount)
+        return self
 
 
 riyad = User("Riyad", 500)
@@ -42,3 +46,5 @@ baraa.display_user_balance()
 riyad.transfer_money(baraa, 300)
 riyad.display_user_balance()
 baraa.display_user_balance()
+
+riyad.make_deposit(300).make_withdrawal(200).make_deposit(300).display_user_balance()
